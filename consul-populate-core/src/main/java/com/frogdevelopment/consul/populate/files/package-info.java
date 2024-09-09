d938cdbd97bb27a229871c0899b997ce2c502a8e
Le Gall, Benoit <legall.benoit@gmail.com>
2024-09-09 19:58:53 +0200

    [git] add git repo & consul-populate-server

--- consul-populate-core/src/main/java/com/frogdevelopment/consul/populate/files/package-info.java
+++ consul-populate-core/src/main/java/com/frogdevelopment/consul/populate/files/package-info.java
@@ -4,9 +4,5 @@
  * @author Le Gall Benoît
  * @since 1.0.0
  */
-@Configuration
-@Requires(property = "consul.files")
 package com.frogdevelopment.consul.populate.files;
 
-import io.micronaut.context.annotation.Configuration;
-import io.micronaut.context.annotation.Requires;
