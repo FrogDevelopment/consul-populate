68beedc46e98ca5c1cd80fd4c3e60b4ae60ef7dc
Le Gall, Benoit <legall.benoit@gmail.com>
2025-11-18 22:45:54 +0100

    [git] move to its own separated extension

--- consul-populate-server/build.gradle.kts
+++ consul-populate-server/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("io.micronaut.minimal.application") version "4.5.3"
-    id("com.frogdevelopment.jreleaser.publish-convention")
-    alias(libs.plugins.jib)
+//    alias(libs.plugins.jib)
 }
 
 micronaut {
@@ -14,73 +13,47 @@ micronaut {
 }
 
 dependencies {
-    annotationProcessor(mn.lombok)
-
-    implementation(projects.consulPopulateCore)
-    implementation("commons-io:commons-io:2.17.0")
-
-    compileOnly(mn.lombok)
+    implementation(projects.consulPopulateGit)
 
     runtimeOnly(mn.logback.classic)
     runtimeOnly(mn.snakeyaml)
-
-    testImplementation(libs.jetbrains.annotations)
-    testImplementation(mn.assertj.core)
-    testImplementation(mn.junit.jupiter.params)
-    testImplementation(mn.mockito.junit.jupiter)
-    testImplementation(libs.testcontainers.core)
-    testImplementation(libs.testcontainers.junit)
-    testImplementation(libs.testcontainers.consul)
-    testImplementation("org.awaitility:awaitility:4.2.2")
-    testCompileOnly(mn.lombok)
 }
 
 application {
     mainClass.set("com.frogdevelopment.consul.populate.ConsulPopulateApplication")
 }
 
-publishing {
-    publications {
-        named<MavenPublication>("mavenJava") {
-            pom {
-                name = "Consul Populate - Server"
-                description = "Server library for Consul Populate"
-            }
-        }
-    }
-}
-
-jib {
-    from {
-        image = "eclipse-temurin:21-jre-alpine"
-        platforms {
-            platform {
-                architecture = "amd64"
-                os = "linux"
-            }
-            platform {
-                architecture = "arm64"
-                os = "linux"
-            }
-        }
-    }
-
-    to {
-        image = "frogdevelopment/${name}:${rootProject.version}"
-        if (System.getenv("CI") == "true") {
-            auth {
-                username = System.getenv("DOCKER_USR")
-                password = System.getenv("DOCKER_PSW")
-            }
-        }
-    }
-
-    container {
-        setMainClass(application.mainClass)
-
-        jvmFlags = listOf("-Xmx128m")
-        volumes = listOf("/tmp")
-        creationTime = "USE_CURRENT_TIMESTAMP"
-        labels.put("frog.image_base", jib.from.image.toString())
-    }
-}
+//jib {
+//    from {
+//        image = "eclipse-temurin:21-jre-alpine"
+//        platforms {
+//            platform {
+//                architecture = "amd64"
+//                os = "linux"
+//            }
+//            platform {
+//                architecture = "arm64"
+//                os = "linux"
+//            }
+//        }
+//    }
+//
+//    to {
+//        image = "frogdevelopment/${name}:${rootProject.version}"
+//        if (System.getenv("CI") == "true") {
+//            auth {
+//                username = System.getenv("DOCKER_USR")
+//                password = System.getenv("DOCKER_PSW")
+//            }
+//        }
+//    }
+//
+//    container {
+//        setMainClass(application.mainClass)
+//
+//        jvmFlags = listOf("-Xmx128m")
+//        volumes = listOf("/tmp")
+//        creationTime = "USE_CURRENT_TIMESTAMP"
+//        labels.put("frog.image_base", jib.from.image.toString())
+//    }
+//}
