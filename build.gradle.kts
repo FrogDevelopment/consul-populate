0f9834e2260ef615430c7450d58c44f404e25091
Le Gall, Benoit <legall.benoit@gmail.com>
2025-12-01 22:37:54 +0100

    [sonar] run Sonar manually to have Coverage

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     java
     id("com.frogdevelopment.jreleaser.deploy-convention")
+    id("org.sonarqube") version "7.1.0.6387"
+    jacoco
 }
 
 version = "1.2.0-SNAPSHOT"
@@ -16,3 +18,37 @@ java {
         vendor = JvmVendorSpec.ADOPTIUM
     }
 }
+
+sonar {
+    properties {
+        property("sonar.projectKey", "FrogDevelopment_consul-populate")
+        property("sonar.organization", "frogdevelopment")
+        property("sonar.junit.reportPaths", "build/test-results/test/")
+    }
+}
+
+testing {
+    suites {
+        // Configure the built-in test suite
+        val test by getting(JvmTestSuite::class) {
+            // Use JUnit Jupiter test framework
+            useJUnitJupiter(mn.versions.junit5)
+        }
+    }
+}
+
+tasks {
+    test {
+        finalizedBy(jacocoTestReport)
+    }
+
+    jacocoTestReport {
+        dependsOn(test)
+
+        reports {
+            xml.required.set(true)
+            html.required.set(false)
+        }
+    }
+}
+
